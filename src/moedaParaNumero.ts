f6ae5c464db76abb70072f52d262ca4b06973902
irailtonreis <irailton.oliveira91@gmail.com>
2023-07-04 18:28:35 -0300

    feat: Normaliza dados da api

---
+++ src/moedaParaNumero.ts
@@ -0,0 +1,10 @@
+/**
+ * 
+ * Recebe string '1.200,50' retorna number 1200.50
+ */
+
+export default function moedaParaNumero(moeda: string): number | null {
+    const numero = Number(moeda.replaceAll('.', '').replaceAll(',', '.'))
+    return isNaN(numero) ? null : numero
+
+}
--- src/script.ts
+++ src/script.ts
@@ -1,26 +1,11 @@
 import fetchData from "./fetchData.js";
-type TransacaoPagamento = "Boleto" | "Cartão de Crédito"
-type TransacaoStatus =  "Paga" | "Recusada pela operadora de cartão" | "Aguardando pagamento"| "Estornada";
-interface TransacaoAPI{
-    Nome: string;
-    ID: number;
-    Data: string;
-    Status: TransacaoStatus;
-    Email: string;
-    ["Valor (R$)"]: string;
-    ["Cliente Novo"]: number;
-    ["Forma de Pagamento"]: TransacaoPagamento;
-}
-
+import normalizarTransacao from "./normalizarTransacao.js";
 async function handleData(){
-    const data = await fetchData<TransacaoAPI[]>("https://api.origamid.dev/json/transacoess.json")
-    console.log(data)
-
-    if(data){
-        data.forEach((item)  => {
-            console.log(item["Cliente Novo"])
-        });
-    }
+    const data = await fetchData<TransacaoAPI[]>("https://api.origamid.dev/json/transacoes.json")
+    if(!data) return
+    const transacoes = data.map(normalizarTransacao)
+    console.log(transacoes)
+    
 }
 
 handleData()
