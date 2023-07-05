df94f8fd020f9d831846d1f6e5609d7d6724837d
irailtonreis <irailton.oliveira91@gmail.com>
2023-07-05 11:56:19 -0300

    feat: Preechimento dados estatísticas

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,4 @@
+import Estatisticas from "./Estatisticas.js";
 import fetchData from "./fetchData.js";
 import normalizarTransacao from "./normalizarTransacao.js";
 async function handleData() {
@@ -8,8 +9,19 @@ async function handleData() {
     const transacoes = data.map(normalizarTransacao);
 
     preencherTabela(transacoes);
+    preencherTransacoes(transacoes);
 }
 
+function preencherTransacoes(transacoes: Transacao[]): void {
+    const data = new Estatisticas(transacoes)
+    const totalElement = document.querySelector<HTMLElement>('#total span')
+    if (totalElement) {
+        totalElement.innerText = data.total.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+    }
+}
 function preencherTabela(transacoes: Transacao[]): void {
     const tabela = document.querySelector("#transacoes tbody");
     if (!tabela) return;
